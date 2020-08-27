e0da217b816e9d4ab59a380c7d7ef2d09682a074
PeterGoedeke <peterbgoedeke@gmail.com>
2020-08-27 16:52:16 +1200

    Refactoring to clean code

--- contentScript.js
+++ contentScript.js
@@ -1,49 +1,60 @@
+const latLngSelector = '#reveal-card > div > div.widget-reveal-card-container > button.link-like.widget-reveal-card-lat-lng'
+const exitButtonSelector = '#reveal-card > div > button.widget-reveal-card-close > span'
+
+const geocodeUrl = 'https://us1.locationiq.com/v1/reverse.php?key=dca29e623666fc&format=json&zoom=14'
+const geocodeLookups = ['suburb', 'hamlet', 'locality', 'village', 'town', 'county', 'city']
+
+function closeLatLngDisplay() {
+    setTimeout(() => {
+        document.querySelector(exitButtonSelector).click()
+    }, 100)
+}
+
 const scene = document.querySelector('#scene')
 scene.addEventListener('click', e => {
-    const text = document.querySelector('#reveal-card > div > div.widget-reveal-card-container > button.link-like.widget-reveal-card-lat-lng')
-    console.log(text.textContent)
-    if(text.textContent) appendItem(text.textContent)
-    setTimeout(() => document.querySelector('#reveal-card > div > button.widget-reveal-card-close > span').click(), 100)
-    
-    console.log(list)
+    const latLngDisplay = document.querySelector(latLngSelector)
+
+    if(latLngDisplay.textContent) {
+        addLocation(latLngDisplay.textContent)
+    }
+    closeLatLngDisplay()
 })
 
 const list = {}
 const panel = document.createElement('div')
 panel.className = 'panel'
 
-async function appendItem(latLng) {
+function createLocationElement(latLng) {
     const item = document.createElement('div')
     item.className = 'item'
     item.textContent = latLng
 
     const input = document.createElement('input')
-    item.appendChild(input)
     input.addEventListener('keypress', e => {
         list[latLng] = input.value
     })
+    item.appendChild(input)
     panel.appendChild(item)
+    return input
+}
+
+async function addLocation(latLng) {
+    const input = createLocationElement(latLng)
     panel.scrollTop = panel.scrollHeight
 
-    const url = 'https://us1.locationiq.com/v1/reverse.php?key=dca29e623666fc&format=json&zoom=14'
     const [lat, lon] = latLng.split(',')
-    const request = await fetch(url + `&lat=${lat}&lon=${lon}`)
-    const response = await request.json()
-    console.log(response)
+    const response = await fetch(geocodeUrl + `&lat=${lat}&lon=${lon}`)
+    const result = await response.json()
     
-    const a = response.address
-    const lookups = ['suburb', 'hamlet', 'locality', 'village', 'town', 'county', 'city']
-    for(const name of lookups) {
-        if(a[name]) {
-            input.value = a[name]
-            list[latLng] = a[name]
-            break
-        }
-        else {
-            input.value = 'Undefined value'
-            list[latLng] = 'Undefined value'
+    const a = result.address
+    for(const area of geocodeLookups) {
+        if(a[area]) {
+            input.value = a[area]
+            list[latLng] = a[area]
+            return
         }
     }
-
+    input.value = 'Undefined value'
+    list[latLng] = 'Undefined value'
 }
 document.body.appendChild(panel)
