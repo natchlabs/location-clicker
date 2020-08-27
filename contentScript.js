80113de1c67055ecbd9c03a6fefff2d388b9c6ad
PeterGoedeke <peterbgoedeke@gmail.com>
2020-08-27 16:55:24 +1200

    Commented code

--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,19 @@
+// selectors to get relevant google maps elements
 const latLngSelector = '#reveal-card > div > div.widget-reveal-card-container > button.link-like.widget-reveal-card-lat-lng'
 const exitButtonSelector = '#reveal-card > div > button.widget-reveal-card-close > span'
 
+// url for geocoder and relevant strings to check in the address property of the response
 const geocodeUrl = 'https://us1.locationiq.com/v1/reverse.php?key=dca29e623666fc&format=json&zoom=14'
 const geocodeLookups = ['suburb', 'hamlet', 'locality', 'village', 'town', 'county', 'city']
 
+// close the google maps latLng display; this saves the user a click having to do it themselves every single time
 function closeLatLngDisplay() {
     setTimeout(() => {
         document.querySelector(exitButtonSelector).click()
     }, 100)
 }
 
+// event listener to register relevant clicks and run the script
 const scene = document.querySelector('#scene')
 scene.addEventListener('click', e => {
     const latLngDisplay = document.querySelector(latLngSelector)
@@ -20,16 +24,14 @@ scene.addEventListener('click', e => {
     closeLatLngDisplay()
 })
 
-const list = {}
-const panel = document.createElement('div')
-panel.className = 'panel'
-
+// append an element to the panel representing a location; returns the input so it can be accessed
 function createLocationElement(latLng) {
     const item = document.createElement('div')
     item.className = 'item'
     item.textContent = latLng
 
     const input = document.createElement('input')
+    // allow the user to manually override the geocoder
     input.addEventListener('keypress', e => {
         list[latLng] = input.value
     })
@@ -38,6 +40,7 @@ function createLocationElement(latLng) {
     return input
 }
 
+// add a location to the panel by reading the latLng from google maps and using a geocoder to get the name of the place
 async function addLocation(latLng) {
     const input = createLocationElement(latLng)
     panel.scrollTop = panel.scrollHeight
@@ -46,6 +49,7 @@ async function addLocation(latLng) {
     const response = await fetch(geocodeUrl + `&lat=${lat}&lon=${lon}`)
     const result = await response.json()
     
+    // read the response and get the most local property of the place name possible (i.e. prefer the town name over the city name)
     const a = result.address
     for(const area of geocodeLookups) {
         if(a[area]) {
@@ -54,7 +58,12 @@ async function addLocation(latLng) {
             return
         }
     }
+    // if this fails, give an undefined value
     input.value = 'Undefined value'
     list[latLng] = 'Undefined value'
 }
 document.body.appendChild(panel)
+
+const list = {}
+const panel = document.createElement('div')
+panel.className = 'panel'
